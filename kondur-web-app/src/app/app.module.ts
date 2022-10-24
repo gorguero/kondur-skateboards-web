29df427746360fe2d8ae13b8a1695de68ac2193b
Juan Cruz Aguero Luna <jcaguerodev@gmail.com>
2022-10-24 20:38:28 -0300

    Creando encabezado

--- kondur-web-app/src/app/app.module.ts
+++ kondur-web-app/src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { SharedModule } from './shared/shared.module';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]

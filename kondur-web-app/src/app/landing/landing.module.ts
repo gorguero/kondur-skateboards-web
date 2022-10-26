c9225e9810fb55ebb45e0311af50968bc24c4ffe
Juan Cruz Aguero Luna <jcaguerodev@gmail.com>
2022-10-26 17:53:02 -0300

    Maqutado productos destacados
    Se agrego tambien un fix en los estilos de carrucel

--- kondur-web-app/src/app/landing/landing.module.ts
+++ kondur-web-app/src/app/landing/landing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PrincipalPageComponent } from './principal-page/principal-page.component';
 import { CarrucelComponent } from './contenido-landing/carrucel/carrucel.component';
+import { ProductosDestacadosComponent } from './contenido-landing/productos-destacados/productos-destacados.component';
 
 
 
 @NgModule({
   declarations: [
     PrincipalPageComponent,
-    CarrucelComponent
+    CarrucelComponent,
+    ProductosDestacadosComponent
   ],
   imports: [
     CommonModule

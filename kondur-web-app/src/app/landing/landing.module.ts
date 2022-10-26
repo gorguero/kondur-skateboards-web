797de7fe2a548ed20098800b27f248fbbc4f49a2
Juan Cruz Aguero Luna <jcaguerodev@gmail.com>
2022-10-26 17:35:36 -0300

    Maquetando contenido principal / carrucel

--- kondur-web-app/src/app/landing/landing.module.ts
+++ kondur-web-app/src/app/landing/landing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PrincipalPageComponent } from './principal-page/principal-page.component';
+import { CarrucelComponent } from './contenido-landing/carrucel/carrucel.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PrincipalPageComponent,
+    CarrucelComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    PrincipalPageComponent
   ]
 })
 export class LandingModule { }
---
+++ kondur-web-app/src/app/landing/principal-page/principal-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-principal-page',
+  templateUrl: './principal-page.component.html',
+  styleUrls: ['./principal-page.component.css']
+})
+export class PrincipalPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

70cc70620d1e419fc600b3b983b038b5f32510e5
Juan Cruz Aguero Luna <jcaguerodev@gmail.com>
2022-10-26 17:04:16 -0300

    Maquetado de menu hamburguesa

--- kondur-web-app/src/app/shared/shared.module.ts
+++ kondur-web-app/src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './navbar/menu/menu.component';
 
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    MenuComponent
   ],
   imports: [
     CommonModule

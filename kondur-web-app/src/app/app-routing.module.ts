e52281c213e40ee68414accecdf3c4d1130ccd4a
Juan Cruz Aguero Luna <jcaguerodev@gmail.com>
2022-10-31 20:11:47 -0300

    Maquetado pagina producto

--- kondur-web-app/src/app/app-routing.module.ts
+++ kondur-web-app/src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { PrincipalPageComponent } from './landing/principal-page/principal-page.component';
+import { ProductoComponent } from './tienda/producto/producto.component';
 import { TiendaPageComponent } from './tienda/tienda-page/tienda-page.component';
 
 const routes: Routes = [
   {path: '', redirectTo:'/home', pathMatch: 'full'},
   {path: 'home', component: PrincipalPageComponent},
-  {path: 'tienda', component: TiendaPageComponent}
+  {path: 'tienda', component: TiendaPageComponent},
+  {path: 'producto', component: ProductoComponent}
 ];
 
 @NgModule({

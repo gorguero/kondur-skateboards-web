2406ffac696ddb2fb2a0bf6697a2b454e2c12f38
Juan Cruz Aguero Luna <jcaguerodev@gmail.com>
2022-11-12 11:08:05 -0300

    Maquetando vista Checks

--- kondur-web-app/src/app/app-routing.module.ts
+++ kondur-web-app/src/app/app-routing.module.ts
@@ -4,12 +4,29 @@ import { RouterModule, Routes } from '@angular/router';
 import { PrincipalPageComponent } from './landing/components/principal-page/principal-page.component';
 import { ProductoComponent } from './tienda/pages/producto/producto.component';
 import { TiendaPageComponent } from './tienda/components/tienda-page/tienda-page.component';
+import { ChecksPrincipalComponent } from './checks/components/checks-principal/checks-principal.component';
 
 const routes: Routes = [
-  {path: '', redirectTo:'/home', pathMatch: 'full'},
-  {path: 'home', component: PrincipalPageComponent},
-  {path: 'tienda', component: TiendaPageComponent},
-  {path: 'producto', component: ProductoComponent}
+  {
+    path: '',
+    redirectTo:'/home',
+     pathMatch: 'full'
+  },
+  {
+    path: 'home', 
+    component: PrincipalPageComponent
+  },
+  {
+    path: 'tienda', 
+    component: TiendaPageComponent},
+  {
+    path: 'producto', 
+    component: ProductoComponent
+  },
+  {
+    path: 'checks',
+    component: ChecksPrincipalComponent
+  }
 ];
 
 @NgModule({
--- kondur-web-app/src/app/app.module.ts
+++ kondur-web-app/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingModule } from './landing/landing.module';
 import { TiendaModule } from './tienda/tienda.module';
+import { ChecksModule } from './checks/checks.module';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { TiendaModule } from './tienda/tienda.module';
     AppRoutingModule,
     SharedModule,
     LandingModule,
-    TiendaModule
+    TiendaModule,
+    ChecksModule
   ],
   providers: [],
   bootstrap: [AppComponent]

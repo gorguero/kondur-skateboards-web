54728d6e36ec6d20dcbb50b3d35e2fb49eaab299
gorguero <jcaguerodev@gmail.com>
2023-06-02 20:58:50 -0300

    Contenido administrador
    Faltan ajustar enrutamientos internos

--- kondur-web-app/src/app/app-routing.module.ts
+++ kondur-web-app/src/app/app-routing.module.ts
@@ -4,17 +4,23 @@ import { RouterModule, Routes } from '@angular/router';
 import { LayoutHomeComponent } from './principal/home/layout-home/layout-home.component';
 import { ProductoComponent } from './principal/tienda/components/producto/producto.component';
 import { RegisterComponent } from './sesion/signup/register/register.component';
-import { ProductosComponent } from './admin/productos/productos.component';
-import { FacturasComponent } from './admin/facturas/facturas.component';
-import { ChecksComponent } from './admin/checks/checks.component';
+import { ProductosComponent } from './admin/components/productos/productos.component';
+import { FacturasComponent } from './admin/components/facturas/facturas.component';
+import { ChecksComponent } from './admin/components/checks/checks.component';
 import { PerfilComponent } from './sesion/usuario/perfil/perfil.component';
 import { CarritoComponent } from './shared/header/carrito/carrito.component';
 import { LoginComponent } from './sesion/signin/login/login.component';
 import { LayoutNosotrosComponent } from './principal/nosotros/layout-nosotros/layout-nosotros.component';
 import { LayoutChecksComponent } from './principal/checks/layout-checks/layout-checks.component';
 import { LayoutTiendaComponent } from './principal/tienda/layout-tienda/layout-tienda.component';
+import { LayoutAdminComponent } from './admin/layout-admin/layout-admin.component';
 
 const routes: Routes = [
+  {
+    path: 'administrador',
+    // component: ProductosComponent
+    loadChildren: () => import('./admin/admin.module').then( m => m.AdminModule)
+  },
   {
     path: '',
     loadChildren: () => import('./principal/principal.module').then( m => m.PrincipalModule)
@@ -24,27 +30,6 @@ const routes: Routes = [
     redirectTo:'principal',
     pathMatch: 'full'
   },
-
-  // { //PRINCIPAL
-  //   path: 'home', 
-  //   component: LayoutHomeComponent
-  // },
-  // {//PRINCIPAL
-  //   path: 'tienda', 
-  //   component: LayoutTiendaComponent
-  // },
-  // {//PRINCIPAL
-  //   path: 'producto', 
-  //   component: ProductoComponent
-  // },
-  // {//PRINCIPAL
-  //   path: 'checks',
-  //   component: LayoutChecksComponent
-  // },
-  // {//PRINCIPAL
-  //   path: 'nosotros',
-  //   component: LayoutNosotrosComponent
-  // },
 
   {//SESION
     path: 'signup',
@@ -57,19 +42,6 @@ const routes: Routes = [
   { //SESION
     path: 'perfil',
     component: PerfilComponent
-  },
-
-  { //ADMIN
-    path: 'admin/productos',
-    component: ProductosComponent
-  },
-  {//ADMIN
-    path: 'admin/facturas',
-    component: FacturasComponent
-  },
-  {//ADMIN
-    path: 'admin/checks',
-    component: ChecksComponent
   },
 
   { //SHARED
--- kondur-web-app/src/app/principal/home/layout-home/layout-home.component.ts
+++ kondur-web-app/src/app/principal/home/layout-home/layout-home.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-layout-home',
   templateUrl: './layout-home.component.html',
-  styleUrls: ['./layout-home.component.css']
 })
 export class LayoutHomeComponent {
 

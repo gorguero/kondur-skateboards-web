05380a82964cbaf1485e1c6aa47ef588214c62b7
gorguero <jcaguerodev@gmail.com>
2023-06-02 21:04:30 -0300

    Update app-routing.module.ts

--- kondur-web-app/src/app/app-routing.module.ts
+++ kondur-web-app/src/app/app-routing.module.ts
@@ -1,19 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { LayoutHomeComponent } from './principal/home/layout-home/layout-home.component';
-import { ProductoComponent } from './principal/tienda/components/producto/producto.component';
 import { RegisterComponent } from './sesion/signup/register/register.component';
-import { ProductosComponent } from './admin/components/productos/productos.component';
-import { FacturasComponent } from './admin/components/facturas/facturas.component';
-import { ChecksComponent } from './admin/components/checks/checks.component';
 import { PerfilComponent } from './sesion/usuario/perfil/perfil.component';
 import { CarritoComponent } from './shared/header/carrito/carrito.component';
 import { LoginComponent } from './sesion/signin/login/login.component';
-import { LayoutNosotrosComponent } from './principal/nosotros/layout-nosotros/layout-nosotros.component';
-import { LayoutChecksComponent } from './principal/checks/layout-checks/layout-checks.component';
-import { LayoutTiendaComponent } from './principal/tienda/layout-tienda/layout-tienda.component';
-import { LayoutAdminComponent } from './admin/layout-admin/layout-admin.component';
 
 const routes: Routes = [
   {

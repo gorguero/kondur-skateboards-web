7ed30023cf55f6197243e0cf90fcd6e1979ad6e2
gorguero <jcaguerodev@gmail.com>
2023-06-27 20:02:25 -0300

    Creando primera version de interface usuario

--- kondur-web-app/src/app/models/usuarios.model.ts
+++ kondur-web-app/src/app/models/usuarios.model.ts
@@ -5,12 +5,12 @@ export class Usuarios {
     public nickname: string,
     public email: string,
     public password: string,
-    public repeatPass: string,
-    public direcciones: string[],
-    public creadoEn: Date,
-    public actualizadoEn: Date,
-    public rol: 'ADMIN_ROLE' | 'USER_ROLE',
-    public estado: boolean
+    public repeatPass?: string,
+    public creadoEn?: Date,
+    public actualizadoEn?: Date,
+    public rol?: 'ADMIN_ROLE' | 'USER_ROLE',
+    public estado?: boolean,
+    public direcciones?: string[],
   ) {}
 }
 

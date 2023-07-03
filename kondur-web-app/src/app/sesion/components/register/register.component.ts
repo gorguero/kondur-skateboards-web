9b68350069b11056a623c9f383e8c1c4f306ec9a
gorguero <jcaguerodev@gmail.com>
2023-07-03 15:53:55 -0300

    Nuevos imputos y validaciones
    Se agregaron campos requerido para registrar un usuario + validaciones de los inputs requeridos

--- kondur-web-app/src/app/sesion/components/register/register.component.ts
+++ kondur-web-app/src/app/sesion/components/register/register.component.ts
@@ -7,23 +7,66 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+
   // ESPACIO PARA VALIDACION DE FORMULARIO DE REGISTRO
-  registroForm: FormGroup;
-  constructor(private fb: FormBuilder) {
-    this.registroForm = this.fb.group({
-      nombre: ['', Validators.required],
-      email: ['', Validators.required, Validators.email],
-      usuario: ['', Validators.required],
-      contrasenia: ['', Validators.required, Validators.minLength(8)],
-      repContrasenia: ['', Validators.required],
-      terminos: ['', Validators.required],
-    })
-   }
+  public registroForm: FormGroup = this.fb.group({
+    nombre: ['', [Validators.required, Validators.minLength(3)]],
+    apellido: ['', [Validators.required, Validators.minLength(3)]],
+    email: ['', [Validators.required, Validators.email]],
+    nickname: ['', [Validators.required, Validators.minLength(5)]],
+    password: ['', [Validators.required, Validators.minLength(8)]],
+    repPassword: ['', [Validators.required, Validators.minLength(8)]],
+    calle1: [''],
+    calle2: [''],
+    altura: [''],
+    codPostal: [''],
+    terminos: ['', Validators.required],
+  });
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.registroForm.reset();
   }
 
-  registrarUsuario(){
-    console.log(this.registroForm)
+  /* VALIDACIONES Y MENSAJES DE RESPUESTA */
+  isValidField( field:string ):boolean | null{
+    return this.registroForm.controls[field].errors && this.registroForm.controls[field].touched;
+  }
+
+  getFieldError( field:string ):string | null{
+
+    if( !this.registroForm.controls[field] ) return null;
+
+    const errors = this.registroForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      
+      switch(key){
+        case 'required':
+          return 'Este campo es requerido*';
+        
+        case 'minlength':
+          return `Mínimo ${errors['minlength'].requiredLength} carácteres*`;
+      }
+
+    }
+
+    return null;
+
+  }
+
+  /* FIN VALIDACIONES Y MENSAJES DE RESPUESTA */
+
+  registrarUsuario():void{
+    
+    if( this.registroForm.invalid ) {
+      this.registroForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.registroForm.value);
+
+    this.registroForm.reset();
+
   }
 }

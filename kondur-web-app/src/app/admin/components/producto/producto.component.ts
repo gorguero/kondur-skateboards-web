b46e6e9da1825f7dfed478c6e4b073b29295e65e
Marcos Quinteros <104270568+M4gnus99@users.noreply.github.com>
2023-10-03 13:05:56 -0300

    Se agrego talla e iteracion de la tienda principal
    Se agrego talla a la estructura de producto, tambien se edito el formulario de productos para poder admitir el nuevo atributo. Falta agregar el editar con el nuevo producto y revisar las validaciones tanto del agregar como el editar.
    Tambien se agrego la iteracion de producto es la tienda, faltan las sub categorias

--- kondur-web-app/src/app/admin/components/producto/producto.component.ts
+++ kondur-web-app/src/app/admin/components/producto/producto.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
-import { Producto } from 'src/app/models/producto.model';
+import { Producto,Talla } from 'src/app/models/producto.model';
 import { ProductoService } from 'src/app/services/producto.service';
 
 @Component({
@@ -60,36 +60,36 @@ export class ProductoComponent {
           descripcion:data.descripcion,
           imagen:data.imagen,
           precio:data.precio,
-          stock:data.stock,
+          // stock:data.stock,
           categoria:data.categoria,
-          talle:data.talle,
-          medida:data.medida,
+        //   talle:data.talle,
+        //   medida:data.medida,
         })
       })
     }
   }
   
-  editarProducto(){
-    const PRODUCTO: Producto ={
-      nombreProducto: this.productoForm.get('nombreProducto')?.value,
-      descripcion: this.productoForm.get('descripcion')?.value,
-      imagen: this.productoForm.get('imagen')?.value,
-      precio: this.productoForm.get('precio')?.value,
-      stock: this.productoForm.get('stock')?.value,
-      categoria: this.productoForm.get('categoria')?.value,
-      talle: this.productoForm.get('talle')?.value,
-      medida: this.productoForm.get('medida')?.value
-    }
-    if(this.id !== null){
-      console.log(PRODUCTO);
-      this._productoService.editarProducto(this.id, PRODUCTO).subscribe(data =>{
-      this.toastr.info('Se actualizo el producto exitosamente!', 'Producto actualizado');
-      }, error => {
-        console.log(error);
-        this.productoForm.reset();}
-      )
-    }
-  }
+  // editarProducto(){
+  //   const PRODUCTO: Producto ={
+  //     nombreProducto: this.productoForm.get('nombreProducto')?.value,
+  //     descripcion: this.productoForm.get('descripcion')?.value,
+  //     imagen: this.productoForm.get('imagen')?.value,
+  //     precio: this.productoForm.get('precio')?.value,
+  //     // stock: this.productoForm.get('stock')?.value,
+  //     categoria: this.productoForm.get('categoria')?.value,
+  //     // talle: this.productoForm.get('talle')?.value,
+  //     // medida: this.productoForm.get('medida')?.value
+  //   }
+  //   if(this.id !== null){
+  //     console.log(PRODUCTO);
+  //     this._productoService.editarProducto(this.id, PRODUCTO).subscribe(data =>{
+  //     this.toastr.info('Se actualizo el producto exitosamente!', 'Producto actualizado');
+  //     }, error => {
+  //       console.log(error);
+  //       this.productoForm.reset();}
+  //     )
+  //   }
+  // }
   deshabilitarProducto(){
     if (this.id !== null){
       this._productoService.deshabilitarProducto(this.id).subscribe(
--- kondur-web-app/src/app/admin/pages/productos/productos.component.ts
+++ kondur-web-app/src/app/admin/pages/productos/productos.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { AbstractControl, FormBuilder, FormGroup, ValidatorFn, Validators} from '@angular/forms';
+import { AbstractControl, FormArray, FormBuilder, FormGroup, ValidatorFn, Validators} from '@angular/forms';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
-import { Producto } from 'src/app/models/producto.model';
+import { Producto, Talla } from 'src/app/models/producto.model';
 import { ProductoService } from 'src/app/services/producto.service';
 
 @Component({
@@ -14,98 +14,109 @@ export class ProductosComponent {
   categoriaSeleccionada: string;
   input1Habilitado: boolean;
   input2Habilitado: boolean;
-
-  
- public productoForm: FormGroup = this.fb.group({
-    nombreProducto:['',Validators.required],
-    descripcion:['',Validators.required],
-    imagen:['',Validators.required],
-    precio:['',Validators.required],
-    stock:['',Validators.required],
-    categoria:['',Validators.required],
-    talle:[''],
-    medida:[''],
-  },{ validator: this.atLeastOneFieldRequired() });
-
   listProductos: Producto[] = []
 
-  // Validacion para los campos talle y medida, activa el formulario si almenos uno esta lleno
-  atLeastOneFieldRequired(): ValidatorFn {
-    return (control: AbstractControl): { [key: string]: boolean } | null => {
-      const medidaValue = control.get('medida')?.value;
-      const talleValue = control.get('talle')?.value;
+  ngOnInit(): void {
+    this.obtenerProductos();
+  }
 
-      // Verificar que al menos uno de los campos tenga valor
-      if ((!medidaValue && !talleValue) || (medidaValue && talleValue)) {
-        return { 'atLeastOneFieldRequired': true };
-      }
-      return null; // Es válido
-    };
-  } 
-  // Valida que haya una categoria seleccionada para mostrar medidas o talle
   constructor(private fb: FormBuilder,
-              private _productoService: ProductoService,
-              private toastr: ToastrService,
-              private router: Router){
-    this.categoriaSeleccionada = '';
-    this.input1Habilitado = false;
-    this.input2Habilitado = false;
-  }
-  onSelectChange(event: Event): void {
-    const selectElement = event.target as HTMLSelectElement;
-    this.categoriaSeleccionada = selectElement.value;
+    private _productoService: ProductoService,
+    private toastr: ToastrService
+    ){
+      this.categoriaSeleccionada = '';
+      this.input1Habilitado = false;
+      this.input2Habilitado = false;
+    }
+  public productoForm: FormGroup = this.fb.group({
+    nombreProducto: ['', Validators.required],
+    descripcion: ['', Validators.required],
+    imagen: ['', Validators.required],
+    precio: ['', Validators.required],
+    categoria: ['', Validators.required],
+     tallas: this.fb.array([]),
+     
+  }, { });
 
-    if (this.categoriaSeleccionada === 'indumentaria') {
-      this.input2Habilitado = true;
-      this.input1Habilitado = false;
-    }
-    if (this.categoriaSeleccionada === 'lijas') {
-      this.input1Habilitado = true;
-      this.input2Habilitado = false;
-    }
-    if (this.categoriaSeleccionada === 'tablas') {
-      this.input1Habilitado = true;
-      this.input2Habilitado = false;
-    }
-    if (this.categoriaSeleccionada === '') {
-      this.input1Habilitado = false;
-      this.input2Habilitado = false;
-    }
+  get tallas(){
+    return this.productoForm.get('tallas') as FormArray;
   }
+
+  // Agregar una nueva talla o medida en el form
+  agregarTalla() {
+    const tallaFormGroup = this.fb.group({
+      nombre: [''],
+      cantidad: ['']
+    });
+  
+    this.tallas.push(tallaFormGroup);
+  }
+
+  // Eliminar una talla o medida específica por índice
+  eliminarTalla(index: number) {
+    const tallas = this.productoForm.get('tallas') as FormArray;
+    tallas.removeAt(index);
+  }
+
+  
+  // funcion que me permite validar si una de las
+  // dos opciones de categoria  esta seleccionado
+  // onSelectChange(event: Event): void {
+  //   const selectElement = event.target as HTMLSelectElement;
+  //   this.categoriaSeleccionada = selectElement.value;
+
+  //   if (this.categoriaSeleccionada === 'indumentaria') {
+  //     this.input2Habilitado = true;
+  //     this.input1Habilitado = false;
+  //   }
+  //   if (this.categoriaSeleccionada === 'lijas') {
+  //     this.input1Habilitado = true;
+  //     this.input2Habilitado = false;
+  //   }
+  //   if (this.categoriaSeleccionada === 'tablas') {
+  //     this.input1Habilitado = true;
+  //     this.input2Habilitado = false;
+  //   }
+  //   if (this.categoriaSeleccionada === '') {
+  //     this.input1Habilitado = false;
+  //     this.input2Habilitado = false;
+  //   }
+  // }
+  
   //resetea el formulario al apretar cancelar
   resetearFormulario() {
     this.productoForm.reset();
   }
-  ngOnInit(): void {
-    this.obtenerProductos();
-    
-  }
 
-  agregarProducto(){
-    console.log(this.productoForm);
-    console.log(this.productoForm.get('producto')?.value);
+  agregarProducto() {
+    // Acceder a los valores del formulario
+    const nombreProducto = this.productoForm.get('nombreProducto')?.value;
+    const descripcion = this.productoForm.get('descripcion')?.value;
+    const imagen = this.productoForm.get('imagen')?.value;
+    const precio = this.productoForm.get('precio')?.value;
+    const categoria = this.productoForm.get('categoria')?.value;
+    const tallas = this.productoForm.get('tallas')?.value as Talla[];
+  
+  
+    // Crear un objeto de Producto
+    const PRODUCTO: Producto = {
+      nombreProducto,
+      descripcion,
+      imagen,
+      precio,
+      categoria,
+      tallas// Puedes establecer la fecha de creación
+    };
 
-    const PRODUCTO: Producto ={
-      nombreProducto: this.productoForm.get('nombreProducto')?.value,
-      descripcion: this.productoForm.get('descripcion')?.value,
-      imagen: this.productoForm.get('imagen')?.value,
-      precio: this.productoForm.get('precio')?.value,
-      stock: this.productoForm.get('stock')?.value,
-      categoria: this.productoForm.get('categoria')?.value,
-      talle: this.productoForm.get('talle')?.value,
-      medida: this.productoForm.get('medida')?.value
-
-    }
-    console.log(PRODUCTO);
     this._productoService.guardarProducto(PRODUCTO).subscribe(data =>{
       this.toastr.success('Se agrego un producto exitosamente!', 'Producto agregado');
-      window.location.reload();
+      // window.location.reload();
     }, error => {
       console.log(error);
       this.productoForm.reset();}
-    )
+      )
+      console.log(PRODUCTO);
   }
-
   obtenerProductos(){
     this._productoService.getProducto().subscribe(data =>{
       // console.log(data);

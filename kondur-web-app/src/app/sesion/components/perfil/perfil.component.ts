f80ace03746fdc1b10e0413cb4c780f347dfeb35
gorguero <jcaguerodev@gmail.com>
2023-10-30 12:32:26 -0300

    Obteniendo usuario y mostrando datos en Mi Perfil

--- kondur-web-app/src/app/sesion/components/perfil/perfil.component.ts
+++ kondur-web-app/src/app/sesion/components/perfil/perfil.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Usuarios from 'src/app/models/usuarios.model';
+import { AuthService } from 'src/app/services/auth.service';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.component.html',
   styleUrls: ['./perfil.component.css']
 })
-export class PerfilComponent {
+export class PerfilComponent implements OnInit {
+
+  public tokenDecoded = localStorage.getItem('token');
+  private token: any = '';
+  public usuarioActual?: any = {};
+  public usuarioDirecciones?: any = [];
+
+  constructor(private auth: AuthService, private usuarioService: UsuarioService) {
+    this.token = this.auth.getDecodedToken(this.tokenDecoded);
+    
+  }
+
+  ngOnInit(): void {
+    this.usuarioService.obtenerUsuario(this.token.id)
+      .subscribe(data => {
+        let resp:any = data;
+        this.usuarioActual = resp.userById;
+        this.usuarioDirecciones = this.usuarioActual.direcciones;
+        console.log(this.usuarioDirecciones[0])
+      });
+  }
+
+  getToken() {
+    return this.token;
+  }
 
 }

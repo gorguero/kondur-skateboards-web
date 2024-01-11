f0c92e9e02be4d50a103b1a49ce9b7692008447c
Marcos Quinteros <104270568+M4gnus99@users.noreply.github.com>
2024-01-11 17:44:20 -0300

    Carrito, tienda y categorias
    Se termino de ajustar las vistas de la tienda principal, como también la vista de la tienda por categoría
    También elimino el botón para comprar desde la miniatura ya para evitar problemas con la cantidad en el carrito.
    Se termino de configurar bien la selección de categoría y la habilitación el botón de agregar a carrito desde la vista de producto.
    Puntos relevantes de carrito a revisar:
    -Borrado individual de producto, al actualizar vuelven.
    -Borrado total del carrito que borra sesion de usuario

--- kondur-web-app/src/app/principal/tienda/pages/indumentaria-page/indumentaria-page.component.ts
+++ kondur-web-app/src/app/principal/tienda/pages/indumentaria-page/indumentaria-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { ItemCarrito } from 'src/app/models/itemCarrito.model';
 import { Producto } from 'src/app/models/producto.model';
 import { ProductoService } from 'src/app/services/producto.service';
 
@@ -10,7 +12,7 @@ import { ProductoService } from 'src/app/services/producto.service';
 export class IndumentariaPageComponent {
 
   indumentariaList:Producto[] = [];
-  constructor(private _productoService:ProductoService){}
+  constructor(private _productoService:ProductoService,  private toastr: ToastrService){}
 
   ngOnInit(): void {
     this.obtenerProductos();
@@ -25,5 +27,34 @@ export class IndumentariaPageComponent {
     }, error =>{
       console.log(error);
     })
+  }
+  agregarCarrito(producto: Producto){
+    if (producto._id) { // Verificación de nulidad
+      let iCarrito: ItemCarrito ={
+        nombreProducto: producto.nombreProducto,
+        imagen: producto.imagen,
+        precio: producto.precio,
+        cantidad: 1,
+        _id: producto._id
+      }
+      if(localStorage.getItem("carrito") === null){
+        let carrito: ItemCarrito[]=[];
+        carrito.push(iCarrito);
+        localStorage.setItem("carrito", JSON.stringify(carrito));
+      } else {
+        let carritoStorage = localStorage.getItem("carrito") as string;
+        let carrito = JSON.parse(carritoStorage);
+        carrito.push(iCarrito);
+        localStorage.setItem("carrito", JSON.stringify(carrito));
+      }
+    } else {
+      // Manejar el caso donde _id es undefined
+      console.error('El producto no tiene un _id definido:', producto);
+    }
+    this.toastr.success('Producto agregado al carrito', 'Éxito', {
+      timeOut: 3000,  // Duración del mensaje (en milisegundos)
+      progressBar: true,  // Muestra la barra de progreso
+      closeButton: true,  // Muestra el botón de cerrar
+    });
   }
 }

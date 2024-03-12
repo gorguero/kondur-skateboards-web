02931d1928d945ba3ee1dfd276c0bc5765dc5dc0
gorguero <jcaguerodev@gmail.com>
2024-03-12 11:47:05 -0300

    Eliminación de productos
    Se filtra los productos que se repiten en el carrito

--- kondur-web-app/src/app/shared/header/carrito/carrito.component.ts
+++ kondur-web-app/src/app/shared/header/carrito/carrito.component.ts
@@ -13,35 +13,39 @@ export class CarritoComponent {
 
     ngOnInit(): void {
       this.cargarCarrito();
-      console.log(this.listaItemsCarrito)
     }
 
     vaciarCarrito(){
       localStorage.clear();
       this.listaItemsCarrito = [];
+      localStorage.setItem("carrito", JSON.stringify(this.listaItemsCarrito));
       this.calcularMontoTotal(); // Agrega esta línea para recalcular el monto total
     }
     
     private cargarCarrito() {
       let carritoStorage = localStorage.getItem("carrito") as string;
-      let carrito = JSON.parse(carritoStorage);
+      let carrito = this.eliminarDuplicados( JSON.parse(carritoStorage) );
       this.listaItemsCarrito = carrito || [];
 
       // Calcula el monto total al cargar el carrito
       this.calcularMontoTotal();
     }
+    
+
     calcularMontoTotal() {
       // Suma los totales de los productos en el carrito
       this.montoTotal = this.listaItemsCarrito?.reduce((total, producto) => {
         return total + (producto.precio * producto.cantidad);
       }, 0) || 0;
     }
+
     eliminarProducto(id: string | undefined) {
       if (id) {
-        // Realiza la lógica para eliminar el producto con el id proporcionado
-        // Puedes usar el método filter para filtrar el producto que coincide con el id
+        
         this.listaItemsCarrito = this.listaItemsCarrito?.filter(producto => producto._id !== id);
-
+        if( this.listaItemsCarrito?.length === 0){
+          this.vaciarCarrito();
+        }
         // Vuelve a calcular el monto total después de eliminar el producto
         this.calcularMontoTotal();
       } else {
@@ -51,5 +55,13 @@ export class CarritoComponent {
 
     private guardarCarritoEnLocalStorage() {
       localStorage.setItem("carrito", JSON.stringify(this.listaItemsCarrito));
+    }
+
+    private eliminarDuplicados(carritoList: any[]): any[] {
+      return carritoList.filter((item, index, self) =>
+          index === self.findIndex((t) => (
+              t._id === item._id 
+          ))
+      );
     }
 }

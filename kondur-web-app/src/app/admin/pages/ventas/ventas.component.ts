7d11f0e023ac12031cba75f50dcf55935d08fcb2
gorguero <jcaguerodev@gmail.com>
2024-05-15 11:48:13 -0300

    Implementación de servicio para Ventas-Admin
    Se implemento el servicio GetVentasPaginadas para obtener un listado de ventas de manera paginada para mostrar en el administrador - ventas

--- kondur-web-app/src/app/admin/pages/ventas/ventas.component.ts
+++ kondur-web-app/src/app/admin/pages/ventas/ventas.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CargarVentas, VentasServiceResp } from 'src/app/interfaces/cargar-ventas.interface';
+import { VentaServices } from 'src/app/services/venta.service';
 
 @Component({
   selector: 'app-ventas',
   templateUrl: './ventas.component.html',
   styleUrls: ['./ventas.component.css']
 })
-export class VentasComponent{
+export class VentasComponent implements OnInit{
+
+  listVentasPaginated:CargarVentas[] = [];
+  totalVentas:number = 0;
+  desde:number = 0;
   
+  constructor(private _ventasService:VentaServices){}
+
+  ngOnInit(): void {
+    this.cargarVentas();
+  }
+
+  //Carga productos paginados
+  cargarVentas(){
+    this._ventasService.getVentasPaginadas(this.desde)
+      .subscribe({
+        next: (resp:VentasServiceResp) => {
+          this.listVentasPaginated = resp.ventas;
+          this.totalVentas = resp.totalVentas;
+        },
+        error: (err) => console.log(err)    
+      });
+  }
+  cambiarPagina( valor:number ){
+    this.desde += valor;
+
+    if( this.desde < 0 ){
+      this.desde = 0;
+    }else if( this.desde >= this.totalVentas ){
+      this.desde -= valor;
+    }
+
+    this.cargarVentas();
+  }
+
 }
--- kondur-web-app/src/app/services/venta.service.ts
+++ kondur-web-app/src/app/services/venta.service.ts
@@ -3,17 +3,22 @@ import { Injectable, Output, EventEmitter } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
 import { Venta } from '../models/venta.model';
+import { VentasServiceResp } from '../interfaces/cargar-ventas.interface';
 
 const url_base = environment.url_base;
 @Injectable({
   providedIn: 'root'
 })
 export class VentaServices{
-    private url = 'http://localhost:5000/api/ventas/';
+    private url = 'http://localhost:5000/api/ventas';
 
     constructor(private http: HttpClient) { }
 
     createVenta(venta: Venta): Observable<any>{
       return this.http.post(this.url, venta);
+    }
+
+    getVentasPaginadas( desde:number = 0 ){
+      return this.http.get<VentasServiceResp>(`${this.url}?desde=${desde}`);
     }
 }
